a22bdece724a8e10d26055ce7ae0781dc9c66ed1
Dennis Mithamo <dennisbmithamo@gmail.com>
2020-10-09 14:30:31 +0300

    feat(resume-page): upload resume as pdf on resume page (#6)
    Co-authored-by: dmithamo <dennis@novek.io>

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -2,20 +2,41 @@
 import React from 'react';
 import { jsx, css } from '@emotion/core';
 import DefaultLayout from '../components/layouts/DefaultLayout';
+import { Document, Page, pdfjs } from 'react-pdf';
+import resumepdf from '../../static/resume.pdf';
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 export default function Resume() {
+  function onSuccess() {}
+
+  function onError(error) {}
+
   return (
     <DefaultLayout>
       <span
         css={css`
           width: 100%;
-          height: 90vh;
           display: flex;
           justify-content: center;
           align-items: center;
+
+          .resume-pdf {
+            width: 80%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            overflow: auto;
+          }
         `}
       >
-        And here will be a pdf of my resume
+        <Document
+          className="resume-pdf"
+          file={resumepdf}
+          onLoadError={onError}
+          onLoadSuccess={onSuccess}
+        >
+          <Page pageNumber={1} />
+        </Document>
       </span>
     </DefaultLayout>
   );
