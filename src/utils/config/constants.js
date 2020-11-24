f56df21f3f015a395a0c798f2bc138cd63a337fe
dmithamo <dennisbmithamo@gmail.com>
2020-11-24 19:30:55 +0300

    update list of roles held

--- src/utils/config/constants.js
+++ src/utils/config/constants.js
@@ -4,6 +4,30 @@ export default {
 
 export const jobsHeld = [
   {
+    title: 'Frontend Engineer',
+    company: 'Proto Energy',
+    website: 'https://protoenergy.com',
+    period: 'Nov 2020 - Present',
+    description:
+      'I collaborate with the UI/UX team in building cool and intutive UIs for diverse internal and customer-facing tools',
+    roles: [
+      'I write performant,testable and maintainable code, primarily in React, with Redux for state management',
+      'I document internal and customer-facing tools to facilitate training on usage as well as maintenance',
+      'I mentor junior devs on the team towards helping building a wholly equipped engineering team',
+      '...(I am sure there will be more here)',
+    ],
+    techstack: [
+      'javascript',
+      'reactjs',
+      'redux',
+      'jest',
+      'react-testing-library',
+      'cypress',
+    ],
+    skills: [],
+  },
+
+  {
     title: 'Software Engineer',
     company: 'Novek',
     website: 'https://www.novek.io/',
@@ -49,5 +73,78 @@ export const jobsHeld = [
       'enzyme',
     ],
     skills: [],
+  },
+];
+
+export const projectsOnCourse = [
+  {
+    title: 'The Timeline',
+    description:
+      "A web app designed to visualize a user's growth by reading their Git(Hub,Lab...) profile(s)",
+    website: 'https://the-timeline.netlify.app',
+    repo: 'https://github.com/dmithamo/the-timeline',
+    screenshot: '',
+    techtack: [
+      'javascript',
+      'typescript',
+      'reactjs',
+      'redux',
+      'html/css',
+      'styled-components',
+    ],
+  },
+  {
+    title: 'The Timeline (API)',
+    description:
+      "The API for a web app designed to visualize a user's growth by reading their Git(Hub,Lab...) profile(s)",
+    website: '',
+    repo: 'https://github.com/dmithamo/timelineapi',
+    screenshot: '',
+    techtack: ['go'],
+  },
+  {
+    title: 'The Budget',
+    description:
+      "A native application designed to manage a user's expenses and budgets",
+    website: '',
+    repo: 'https://github.com/dmithamo/thebudget',
+    screenshot: '',
+    techtack: [
+      'javascript',
+      'react-native',
+      'redux',
+      'html/css',
+      'styled-components',
+    ],
+  },
+  {
+    title: 'The Budget',
+    description:
+      "A native application designed to manage a user's expenses and budgets",
+    website: '',
+    repo: 'https://github.com/dmithamo/the-budget',
+    screenshot: '',
+    techtack: [
+      'javascript',
+      'reactjs',
+      'redux',
+      'html/css',
+      'styled-components',
+    ],
+  },
+  {
+    title: 'GitHub Stats',
+    description:
+      "A web app designed to offer an in-a-nutshell overview of a dev's profile on GitHub.",
+    website: '',
+    repo: 'https://github.com/dmithamo/github-stats',
+    screenshot: '',
+    techtack: [
+      'javascript',
+      'react-native',
+      'redux',
+      'html/css',
+      'styled-components',
+    ],
   },
 ];
